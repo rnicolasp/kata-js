f041927510191adee854d7b51cc7da8a036bd400
Lgarrochoa <91527771+Lgarrochoa@users.noreply.github.com>
2023-03-06 19:54:30 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let botton1 = document.getElementById("button1");
-let resultat = document.getElementById("resultat");
-let input = document.getElementById("input");
-
-botton1.onclick = function(){
-    let resultat1 = input.value.replaceAll(" ", "").toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-    resultat.value = resultat1;
-}
-
